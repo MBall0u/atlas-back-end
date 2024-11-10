e5c209e280c8e820c15182c930a22ba6a43b0114
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-10 09:44:50 -0600

    M - 9:36 AM - made the file gather_data from_an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,8 +25,7 @@ def request_processor():
 
     employee_get = requests.get(
         "https//jsonplaceholder.typicode.com/user/{}".format(employee_id))
-    tasks_get = requests.get(
-        "https//jsonplaceholder.typicode.com/todos")
+    tasks_get = requests.get("https//jsonplaceholder.typicode.com/todos")
 
     if employee_get.status_code != 200 or tasks_get.status_code != 200:
         print("one or more GET requests have failed")
