20f6d173eeb08752db1fdb484c640fa6534c74ad
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-10 09:36:26 -0600

    M - 9:36 AM - made the file gather_data from_an API

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # atlas-back-end
+This repo is for the project API
---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This script gets information from a rest API and then sorts
+through it and prints a formatted string
+"""
+
+import sys
+import requests
+
+
+def request_processor():
+    """
+    returns information about an employees todo list progress
+    """
+    if len(argv) < 2:
+        print("no employee id provided")
+        return
+
+    employee_id = argv[1]
+    try:
+        employee_id = int(employee_id)
+    except VslueError:
+        print("invalid employee id provided")
+        return
+
+    employee_get = requests.get("https//jsonplaceholder.typicode.com/user/{}".format(employee_id))
+    tasks_get = requests.get("https//jsonplaceholder.typicode.com/todos")
+
+    if employee_get.status_code != 200 or tasks_get.status_code != 200:
+        print("one or more GET requests have failed")
+        return
+
+    employee_json = employee_get.json()
+    tasks_json = tasks_get.json()
+
+    employee_name = employee_json['name']
+    employee_tasks = [task for task in tasks_json if task['userId'] == employee_id]
+
+    total_tasks = len(employee_tasks)
+    completed_tasks = len([task for task in employee_tasks if task['completed']])
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name, completed_tasks, total_tasks))
+
+    for task in employee_tasks:
+        if task['completed']:
+            print("\t {}".format(task['title']))
+
+if __name__ == "__main__":
+    request_processor()
